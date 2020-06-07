873b4d7d33ff1135e35f442b2d104af8c6cfac88
Pettybourgeois <Oremus.mwaya@yahoo.com>
2020-06-07 16:34:30 +0300

    Added local settings import

--- horse/settings.py
+++ horse/settings.py
@@ -144,3 +144,8 @@ LOGOUT_REDIRECT_URL = 'index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+try:
+  from .local_settings import *
+except ImportError:
+  pass
